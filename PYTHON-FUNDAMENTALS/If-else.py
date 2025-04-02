2c2ab5546bd1bb2848cd283431d1fe006528cd4b
Sanjay Vasuniya <sanjayv072004@gmail.com>
2025-04-02 22:05:47 +0530

    python fundamentals

---
+++ PYTHON-FUNDAMENTALS/If-else.py
@@ -0,0 +1,64 @@
+# a= 234566;
+# b =88766;
+# if a>b:
+#     print("a is greater than b ");
+# else:
+    
+#     print("b is greater than a ");
+
+# # THIS PROGRAM  FOR CHECKING WETHER THE PERSON ELIGIBLE FOR VOTE OR NOT
+# print("%%%%%%%%%%%%%%%%%%%%% program for check eligible for vote%%%%%%%%%%%%%%%%%%%%%%%%%")
+# age = int (input("Enter your age "));
+# if age>=18:
+#     print("You are eligible for vote.");
+# else:
+#     print("You are not eligible for vote");
+
+# ## THIS PROGRAM IS FOR  CHECK GRADE 
+# print("%%%%%%%%%%%%%%%%%%%%% program for check your grade%%%%%%%%%%%%%%%%%%%%%%%%%")
+# grade = int(input("Enter your grade :"))
+# if grade>=90:
+#     print("you got A grade");
+# elif(grade>=80):
+#     print("you got B grade");
+# elif(grade>=70):
+#     print("you got C grade");
+# else:
+#     print("you need to study more")
+
+# ## THIS PROGRAM IS FOR CHECKING YOUR ELIGIBILITY OF DRIVING
+# print("%%%%%%%%%%%%%%%%%%%%% program for check eligible for drive %%%%%%%%%%%%%%%%%%%%%%%%%")
+# age = int(input("Enter your age :"))
+# if age>=18:
+#     if(age>=80):
+#         print("you can not drive");
+#     else:
+#         print("you can drive");
+# else:
+#     print("you can not drive");
+
+# print("%%%%%%%%%%%%%%%%%%%%% nested if else %%%%%%%%%%%%%%%%%%%%%%%%%")
+# number1 = int(input("Enter the first number:"));
+# number2 = int(input("Enter the second number:"));
+# number3 = int(input("Enter the third number:"));
+
+# if(number1>number2):
+#     if(number1>number3):
+#         print("first number is greatest");
+#     else:
+#       print("third number is greatest");
+# elif(number2>number3):
+#     print("second number is greatest");
+# else:
+#     print("third number is greatest");
+
+# print("%%%%%%%%%%%%%%%%%%%%% nested if else %%%%%%%%%%%%%%%%%%%%%%%%%")
+# num1 = int(input("Enter the number"));
+# if(num1>0):
+#    print("it is a positive number");
+#    if(num1/2):
+#       print("it is an even number");
+# else:
+#   print("it is an odd number")  
+
+
